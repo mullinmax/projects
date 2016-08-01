d9f3d2fe38b1bc1d15a1bd91646c6da68b326aa4
Maxwell Mullin (ubuntu virtualBox) <mullinmax@gmail.com>
2016-07-31 21:24:41 -0400

    able to read and display file in binary

--- srd/bit_reader.cpp
+++ srd/bit_reader.cpp
@@ -4,15 +4,32 @@
 using namespace std;
 
 
-char * load_file(string input){
-	ifstream file ("example.bin", ios::in|ios::binary|ios::ate);
-	char * memblock;  	
+char * load_file(string input, int & length){
+	ifstream file (input.c_str(), ios::in|ios::binary|ios::ate);
+	char * memblock;
+	streampos size;  	
 	if (file.is_open()){
-		int size = file.tellg();
+		size = file.tellg();
 		memblock = new char [size];
 		file.seekg (0, ios::beg);
    		file.read (memblock, size);
+		length = int(size);
    		file.close();
+	}else{
+		cout << "error opening file" << endl;
 	}
 	return memblock;
+}
+
+bool * char_to_bool(char * input, int char_length, int & bin_length){
+	bool * output;
+	int char_size = 8;
+	output = new bool [char_length * char_size];
+	for(int i = 0; i < char_length; i++){
+		for(int j = 0; j < char_size; j++){
+			output[i*char_size+j] = (input[i] >> j) & 1;
+		}
+	}
+	bin_length = char_length * char_size;
+	return output;
 }
--- srd/main.cpp
+++ srd/main.cpp
@@ -6,6 +6,22 @@
 using namespace std;
 
 int main(){
-	char * file = load_file("test.txt");
-	cout << sizeof(file);
+	int char_length = 0;
+	char * char_file = load_file("test.txt", char_length);
+	cout << "----BEGIN RAW FILE----" << endl;
+	for(int i = 0; i < char_length; i++){
+		cout << char_file[i];
+	}
+	cout << "----END RAW FILE----" << endl;
+
+	int bin_length;
+	bool * bin_file = char_to_bool(char_file, char_length, bin_length);
+	cout << "----BEGIN BINARY FILE----" << endl;
+	for(int i = 0; i < bin_length; i++){
+		cout << bin_file[i];
+		if(i % 8 == 7){
+			cout << " ";
+		}
+	}
+	cout << endl <<"----END BINARY FILE----" << endl;
 }
