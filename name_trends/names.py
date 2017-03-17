ac0ccf517b140d24583eab512b686f161eab9154
maxwell <mullinmax@gmail.com>
2017-03-17 13:21:30 -0400

    making name_trends able to graph multiple names

--- name_trends/names.py
+++ name_trends/names.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 
+#read in Name Data
 years = []
 for i in range(1880,2016):
     print("reading year " + str(i))
@@ -10,24 +11,36 @@ for i in range(1880,2016):
         for row in reader:
             people.append(row)
     years.append(people)
-print (int(years[1880-1880][0]['Number']))            
-plot_data = []
-start = -1
-stop = -1
+
+
+    
+#count total number of births for each year
+total_births = []
 for y in range(len(years)):
     temp = 0
-    found = False;
-    for name in years[y]:
-        if((name['Name'] == "Max" or name['Name'] == "Maxwell") and name['Gender'] == "M"):
-            found = True
-            if(start == -1):
-                start = y
-            stop = y
-            temp = temp + int(name['Number'])
-    if(found):
+    for person in years[y]:
+        if(person['Gender'] == 'F'):
+            temp += int(person['Number']) 
+    total_births.append(temp)
+        
+    
+#count births of target name for each year    
+plot_data = []
+found = False;
+for p in range(0,100):
+    print (years[0][p]['Name'], years[0][p]['Number'])
+    temp = []
+    for y in range(len(years)):
+        for name in years[y]:
+            if(name['Name'] == years[0][p]['Name'] and name['Gender'] == years[0][p]['Gender']):
+            #if((name['Name'] == "Brittney" or name['Name'] == "") and name['Gender'] == "F"):
+                temp.append(int(name['Number'])/total_births[y])
+                break
         plot_data.append(temp)
-print (start+1880, stop+1880)
-plt.plot(plot_data)
-plt.show()           
+for i in range(len(plot_data)):
+    plt.plot(plot_data[i])
+plt.savefig('destination_path.png', format='png', dpi=1000)
+print ("done")
+#plt.show()           
             
             
