0dcb2a9770bd198e239ab93fc89ab58ecc8c0f80
Maxwell Mullin (ubuntu virtualBox) <mullinmax@gmail.com>
2017-04-19 13:06:44 -0400

    huh?

--- program_test/tester.cpp
+++ program_test/tester.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
-#include <vecotr>
+#include <vector>
 #include <string>
+#include <fstream>
 using namespace std;
 
-void load_tests(vector<string> &tests, vector<string> &expected);
+void load_tests(string file, vector<string> &tests, vector<string> &expected);
+void run_tests(vector<string> &tests, vector<string> &expected);
 
 int main(int argc, char *argv[]){
+	string file;	
+	if(argc == 1){
+		cin >> file;
+	} else if (argc == 2){
+		file = argv[1];
+	} else {
+		cout << " Too many arguments " << endl;
+		return 0;
+	}
 	vector<string> tests;
 	vector<string> expected;
-	load_tests(tests, expected);
-	
+	load_tests(file, tests, expected);
+	run_tests(tests, expected);	
 }
 
-void load_tests(vector<string> &tests, vector<string> &expected){
-	
+void load_tests(string file, vector<string> &tests, vector<string> &expected){
+	ifstream fin;
+	fin.open(file);
+	vector<string> lines;
+	string temp;
+	while(!fin.eof()){
+		getline(fin, temp);
+		lines.push_back(temp);
+	}
+}
+
+void run_tests(vector<string> &tests, vector<string> &expected){
+
 }
