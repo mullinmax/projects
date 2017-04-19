bd4df68463df963a3a968db5422aa00c878097ab
Maxwell Mullin (ubuntu virtualBox) <mullinmax@gmail.com>
2017-04-19 12:34:18 -0400

    adding testing framework

---
+++ program_test/tester.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vecotr>
+#include <string>
+using namespace std;
+
+void load_tests(vector<string> &tests, vector<string> &expected);
+
+int main(int argc, char *argv[]){
+	vector<string> tests;
+	vector<string> expected;
+	load_tests(tests, expected);
+	
+}
+
+void load_tests(vector<string> &tests, vector<string> &expected){
+	
+}
